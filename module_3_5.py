562792ce6ad997bd98a0861b536d4af39ad94bde
MalytsevaElena <malytseva.elena@gmail.com>
2024-10-29 17:25:21 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -2,7 +2,10 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) == 1:
-        return first
+        if first == 0:
+            return 1
+        else:    
+            return first
     elif first == 0:
         get_multiplied_digits(int(str_number[1:]))
     else:
